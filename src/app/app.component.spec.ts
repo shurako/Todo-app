f8ac1d932f062ddf0fdefd9b4d303dcc8aad5387
DESKTOP-NCUSJM3\patry <patrykmendel8@gmail.com>
2021-10-17 14:45:51 +0200

    create header component

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -26,10 +26,5 @@ describe('AppComponent', () => {
     expect(app.title).toEqual('Todo');
   });
 
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('Todo app is running!');
-  });
+
 });
